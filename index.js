0a9d326e3df6e97d5b042a2f458f9c74d5c7bda2
Dennell Lucero <dennell.lucero@gmail.com>
2016-10-19 16:30:05 +0000

    spring cleaning

--- index.js
+++ index.js
@@ -40,29 +40,27 @@ app.get('/users/:userId', function(req, res) {
     User.findOne({_id: req.params.userId}, function(err, user) {
         if (err) {
             return res.status(500).json({message: 'Internal Server Errror'});
-        } else if (user) {
-            res.status(200).json(user);
-        } else {
-            res.status(404).json({message: 'User not found'});
         }
+        if (user) {
+            return res.status(200).json(user);
+        }
+        return res.status(404).json({message: 'User not found'});
     });
 });
 
 app.put('/users/:userId', jsonParser, function(req, res) {
-    if (req.body.username) {
-        if (typeof(req.body.username) === 'string') {
-            User.findOneAndUpdate({_id: req.params.userId}, {username: req.body.username}, {upsert: true, new: true}, function(err, user) {
-                if (err) {
-                    return res.status(500).json({message: 'Internal Server Errror'});
-                }
-                res.status(200).json({});
-            });
-        } else {
-            res.status(422).json({message: 'Incorrect field type: username'});
-        }
-    } else {
-        res.status(422).json({message: 'Missing field: username'});
+    if (!req.body.username) {
+        return res.status(422).json({message: 'Missing field: username'});
     }
+    if (typeof(req.body.username) !== 'string') {
+        return res.status(422).json({message: 'Incorrect field type: username'});
+    }
+    User.findOneAndUpdate({_id: req.params.userId}, {username: req.body.username}, {upsert: true, new: true}, function(err, user) {
+        if (err) {
+            return res.status(500).json({message: 'Internal Server Errror'});
+        }
+        return res.status(200).json({});
+    });
 });
 
 app.delete('/users/:userId', function(req, res) {
@@ -77,14 +75,13 @@ app.delete('/users/:userId', function(req, res) {
             if(err) {
                return res.status(500).json({message: 'Internal Server Error'}); 
             }
-            res.status(200).json({});
+            return res.status(200).json({});
         });
     });
 });
 
 //messages
 app.get('/messages', function(req, res) {
-    console.log('req.query ', req.query);
     Message.find(req.query).populate('from to').exec(function(err, messages){
         if(err) {
             return res.status(500).json({message: 'Internal Server Error'});
@@ -129,6 +126,7 @@ app.post('/messages', jsonParser, function(req, res) {
         });
     });
 });
+
 
 app.get('/messages/:messageId', function(req, res) {
     Message.findOne({_id: req.params.messageId}).populate('from to').exec( function(err, message) {
